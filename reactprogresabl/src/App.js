1cf1143539df711992e027f7684e3c90d357ab44
Guillermo <guillecalvo5@gmail.com>
2023-03-13 11:34:40 -0300

    mockFn

--- reactprogresabl/src/App.js
+++ reactprogresabl/src/App.js
@@ -1,7 +1,11 @@
+import { useEffect } from 'react';
 import './App.css';
 import Singform from './components/Singform';
-
+import log from './utilities/mock'
 function App() {
+  useEffect(()=>{
+      log();
+  },[])
   return (
     <div className="App">
       <header className="App-header">
---
+++ reactprogresabl/src/utilities/mock.js
@@ -0,0 +1,11 @@
+import data from "./data.json";
+
+export const login = (codemp, user, pasword) => {
+    console.log(data);
+    return new Promise((resolve) =>
+        setTimeout(() => {
+            //validar los 3 parametros
+
+            //return booleano
+        }, 1200))
+}
