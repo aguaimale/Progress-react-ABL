0b4ed91c2d9cf3a47e0e32f0954f3d7eea244c6b
Agustin <agustinaimale@gmail.com>
2023-03-13 09:50:17 -0300

    form

--- reactprogresabl/src/components/Singform.jsx
+++ reactprogresabl/src/components/Singform.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function Singform() {
   return (
-    <div>Singform</div>
+    <div className='formlog'>
+        <div className='capos'>
+
+            <p>Inicio de sesion</p>
+            <label htmlFor=""> Ingresa E-mail</label>
+            <input type="email" name="" id="" />
+            <label htmlFor="">Ingresa Contraseña</label>
+            <input type="password" name="" id="" />
+
+        </div>
+        <div className='divbotones'>
+            <button>Iniciar Sesion</button>
+        </div>
+
+
+    </div>
   )
 }
 
